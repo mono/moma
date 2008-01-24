a91ba0dc22e6994165597651b23987331795297e
Jonathan Pobst <monkey@jpobst.com>
2008-01-24 14:05:55 +0000

    01-24-2008  Jonathan Pobst  <monkey@jpobst.com>
    	* MainForm.Designer.cs: Set AutoScaleMode to None.
    
    svn path=/trunk/moma/; revision=93793

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -486,8 +486,7 @@ namespace MoMA
 			// 
 			// MainForm
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF (6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
 			this.BackColor = System.Drawing.Color.White;
 			this.ClientSize = new System.Drawing.Size (719, 471);
 			this.Controls.Add (this.OptionalGroupBox);
