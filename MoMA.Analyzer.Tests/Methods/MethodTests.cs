765ad26f6bd83c8de19902a68784f08cfeb5104b
Cristian Stoica <cristianstoica85@gmail.com>
2017-05-03 00:39:15 +0300

    Added more type aliases conversions.
    Fixed test cases.

--- MoMA.Analyzer.Tests/Methods/MethodTests.cs
+++ MoMA.Analyzer.Tests/Methods/MethodTests.cs
@@ -6,8 +6,9 @@ namespace MoMA.Analyzer.Methods.Tests
     public class MethodTests
     {
 
+        [TestCase("System.Int64 MyCompany.MyProduct.MyModule::MyMethod(System.String)", "long MyMethod(string)")]
         [TestCase("System.Int32 MyCompany.MyProduct.MyModule::MyMethod(System.String)", "int MyMethod(string)")]
-        [TestCase("System.Int32 MyCompany.MyProduct.MyModule::MyMethod(System.String, System.Collections.Generic.IDictionary`2<System.String,System.Object>,System.String,System.Object)", "int MyMethod(string, IDictionary`2<string, Object>, string, Object)")]
+        [TestCase("System.Int32 MyCompany.MyProduct.MyModule::MyMethod(System.String, System.Collections.Generic.IDictionary`2<System.String,System.Object>,System.String,System.Object)", "int MyMethod(string, IDictionary`2<string, object>, string, object)")]
         public void ToString(string methodSignature, string expectedResult)
         {
             var method = new Method(methodSignature);
